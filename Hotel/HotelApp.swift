558f4ffa2bee5330ac3c7b6d3a46e1627b88df74
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2023-04-29 12:24:01 +0630

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Nyi Ye Han on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
